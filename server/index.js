a1bf2b7a81a0642e3c6cf282051493432b660785
Mark Romero <markromero@Marks-MacBook-Pro.local>
2019-02-26 19:15:57 -0700

    compelted database step

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const massive = require('massive'); 
 require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
+
+//set up database
+massive(process.env.CONNECTION)
+.then( dbInstance => {
+    app.set('db', dbInstance);
+})
+
 
 const PORT = process.env.PORT;
 
